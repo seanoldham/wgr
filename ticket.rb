3b2c04b7730c0ad24ce9a4214dcbca97fe0a4520
Sean Oldham <seanosaur@me.com>
2015-06-20 14:16:00 -0500

    use classes now

--- ticket.rb
+++ ticket.rb
@@ -1,50 +1,41 @@
-# create new Object
-ticket = Object.new
+#rewrite with Ticket class
+class Ticket
 
-# add data to Object by defining methods "Object.data"
-def ticket.date
-  '01/02/03'
+  def date
+    '01/02/03'
+  end
+
+  def venue
+    'Town Hall'
+  end
+
+  def event
+    "Author's reading"
+  end
+
+  def performer
+    'Mark Twain'
+  end
+
+  def seat
+    'Second Balcony, row J, seat 12'
+  end
+
+  def price
+    5.50
+  end
+
+  def availability_status
+    'sold'
+  end
+
+  def available?
+    false
+  end
+
 end
 
-def ticket.venue
-  'Town Hall'
-end
-
-def ticket.event
-  "Author's reading"
-end
-
-def ticket.performer
-  'Mark Twain'
-end
-
-def ticket.seat
-  'Second Balcony, row J, seat 12'
-end
-
-def ticket.price
-  5.50
-end
-
-def ticket.availability_status
-  'sold'
-end
-
-def ticket.available?
-  false
-end
-
-# No string interpolation
-print 'This ticket is for: '
-print ticket.event + ', at '
-print ticket.venue + ', on '
-puts ticket.date + '.'
-print 'The performer is '
-puts ticket.performer + '.'
-print 'The seat is '
-print ticket.seat + ', '
-print 'and it costs $'
-puts '%.2f.' % ticket.price
+ticket = Ticket.new
 
 # With string interpolation
 print "This ticket is for: #{ticket.event}, at #{ticket.venue}, on #{ticket.date}.\n" +
